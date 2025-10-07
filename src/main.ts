dec1bcaac35b360b77d462363c4ac962cae4329c
Victor Hugo Inacio de Oliveira <108891666+vitoinacio@users.noreply.github.com>
2025-10-07 23:07:35 +0000

    merge

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,3 @@
-
-
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { provideAuth, getAuth } from '@angular/fire/auth';
 import { environment } from './environments/environment';
