83df839b67500b5de93a14f4e144b0ac84913886
Victor Hugo Inacio de Oliveira <108891666+vitoinacio@users.noreply.github.com>
2025-11-24 00:46:56 +0000

    feat: retirei os bugs

--- src/app/pages/profile/security/security.page.ts
+++ src/app/pages/profile/security/security.page.ts
@@ -1,19 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonBackButton, IonButtons, IonButton, IonIcon } from '@ionic/angular/standalone';
+import {
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+  IonBackButton,
+  IonButtons,
+  // IonButton,
+  IonIcon,
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-security',
   templateUrl: './security.page.html',
   styleUrls: ['./security.page.scss'],
   standalone: true,
-  imports: [IonIcon, IonButton, IonButtons, IonBackButton, IonContent, IonHeader, IonTitle, IonToolbar, RouterLink]
+  imports: [
+    IonIcon,
+    // IonButton,
+    IonButtons,
+    IonBackButton,
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    RouterLink,
+  ],
 })
 export class SecurityPage implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
